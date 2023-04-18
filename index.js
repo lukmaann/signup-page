3491681a150af33d344e1b1901cb9349c5c80be4
LNUAKX <lukmaannadaf@gmail.com>
2023-04-18 11:06:52 +0530

    started

---
+++ index.js
@@ -0,0 +1,17 @@
+const express= require("express");
+const bodyparser= require("body-parser");
+const app=express();
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server started at port 3000");
+})
